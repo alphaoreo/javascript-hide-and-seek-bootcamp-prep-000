9ec592000fe445d973500ce6dafcf5cbf097e16f
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-15 23:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getFirstSelector(selector){
+  var finding= document.querySelector(selector)
+  return finding
+}
 
+function nestedTarget(){
+  return document.querySelector("div#nested div.target")
+}
+
+function increaseRankBy(n){
+  var targets = querySelectorAll('ul.ranked-list li')
+  for(var i = 0; )
+}
